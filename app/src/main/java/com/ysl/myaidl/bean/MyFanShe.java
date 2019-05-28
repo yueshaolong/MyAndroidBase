d8bd4cb5787c32d9f0a645ea7dbb66a055519355
yueshaolong <yueshaolong@vsxin.com>
2019-05-28 16:16:23 +0800

    反射

--- app/src/main/java/com/ysl/myaidl/bean/MyFanShe.java
+++ app/src/main/java/com/ysl/myaidl/bean/MyFanShe.java
@@ -12,21 +12,30 @@ public class MyFanShe {
 
     private String name;
     public int age;
+    public static MyData myData;
+    public MyData myData2;
+
+    private static class MyData{
+
+    }
 
     public MyFanShe() {
+        this(null, 0);
     }
 
     public MyFanShe(int age) {
-        this.age = age;
+        this(null, age);
     }
 
     public MyFanShe(String name) {
-        this.name = name;
+        this(name, 0);
     }
 
     public MyFanShe(String name, int age) {
         this.name = name;
         this.age = age;
+        myData = new MyData();
+        myData2 = new MyData();
     }
 
     private String getName() {
@@ -55,61 +64,82 @@ public class MyFanShe {
 
     public static void main(String[] args) {
         MyFanShe myFanShe = new MyFanShe("jj", 12);
-        Class<? extends MyFanShe> aClass = myFanShe.getClass();
-
-        Class<MyFanShe> aClass1 = MyFanShe.class;
 
         try {
+            Class<? extends MyFanShe> aClass = myFanShe.getClass();
+            Class<MyFanShe> aClass1 = MyFanShe.class;
             Class<?> aClass2 = Class.forName("com.ysl.myaidl.bean.MyFanShe");
-//            System.out.println("aClass="+aClass.hashCode()+",aClass1="+aClass1.hashCode()+",aClass2="+aClass2.hashCode());
+            //三个拿到的是同一个对象，hashcode是一样的；
+            System.out.println("aClass="+aClass.hashCode()+",aClass1="+aClass1.hashCode()+",aClass2="+aClass2.hashCode());
 
-//            Field[] fields = aClass2.getFields();//拿到全部公有的属性
-//            for (Field field : fields){
-//                System.out.println("fields:"+field.getName());
-//            }
-//            Field age = aClass2.getField("age");//拿到特定的公有的属性
-//            System.out.println("age:"+age.getName());
-//            Field[] declaredFields = aClass2.getDeclaredFields();//可以拿到全部的属性
-//            for (Field field : declaredFields){
-//                System.out.println("declaredFields:"+field.getName());
-//            }
-//            Field name = aClass2.getDeclaredField("name");//拿到特定的属性，包括公有的和私有的；一般用这个
-//            Object o = aClass2.getConstructor().newInstance();
-//            name.set(o, "kk");
-//            System.out.println("o:"+o);
+            Field[] fields = aClass2.getFields();//拿到全部公有的属性
+            for (Field field : fields){
+                System.out.println("fields:"+field.getName());
+            }
+            System.out.println("==================");
+            Field age = aClass2.getField("age");//拿到特定的公有的属性
+            System.out.println("fields:"+age.getName());
+            System.out.println("==================");
+            Field[] declaredFields = aClass2.getDeclaredFields();//可以拿到全部的属性
+            for (Field field : declaredFields){
+                System.out.println("declaredFields:"+field.getName());
+            }
+            System.out.println("==================");
+            Field name = aClass2.getDeclaredField("name");//拿到特定的属性，包括公有的和私有的；一般用这个
+            System.out.println("fields:"+name.getName());
+            Object o4 = name.get(myFanShe);//获取对象（myFanShe）里面的此字段（name）的值；
+            System.out.println("o4:"+o4);
+            Object o = aClass2.getConstructor().newInstance();
+            name.set(o, "kk");
+            System.out.println("o:"+o);
 
-//            Method[] methods = aClass2.getMethods();
-//            for (Method method : methods){
-//                System.out.println("methods:"+method.getName());
-//            }
-//            Method setName = aClass2.getDeclaredMethod("setName", String.class);
-//            System.out.println("setName:"+setName.getName());
-//            Method getName = aClass2.getDeclaredMethod("getName");
-//            System.out.println("getName:"+getName.getName());
+            Field myData = aClass2.getDeclaredField("myData");
+            System.out.println("fields:"+myData);
+            Object o2 = myData.get(null);//获取对象（）里面的此字段（myData）的值；当myData变量为静态变量时，参数传null；
+            System.out.println("o2:"+o2);
 
-//            Constructor<?>[] declaredConstructors = aClass2.getDeclaredConstructors();
-//            for (Constructor constructor : declaredConstructors){
-//                System.out.println("constructor:"+constructor.toString());
-//            }
+            Field myData2 = aClass2.getDeclaredField("myData2");
+            System.out.println("fields:"+myData2);
+            Object o3 = myData2.get(myFanShe);//获取对象（myFanShe）里面的此字段（myData2）的值；
+            System.out.println("o3:"+o3);
 
-//            Constructor<?> declaredConstructor = aClass2.getDeclaredConstructor();
-//            Object o = declaredConstructor.newInstance();
-//            Method setName = aClass2.getDeclaredMethod("setName", String.class);
-//            setName.setAccessible(true);
-//            setName.invoke(o, "aa");
 
-//            Method setAge = aClass2.getDeclaredMethod("setAge", int.class);
-//            System.out.println("setAge:"+setAge.getName());
-//            setAge.invoke(o, 78);
-//            System.out.println(o);
 
-//            Constructor<?> declaredConstructor1 = aClass2.getDeclaredConstructor(String.class, int.class);
-//            Object o1 = declaredConstructor1.newInstance("sd",1);
-//            System.out.println(o1);
+            Method[] methods = aClass2.getMethods();
+            for (Method method : methods){
+                System.out.println("methods:"+method.getName());
+            }
+            Method setName = aClass2.getDeclaredMethod("setName", String.class);
+            System.out.println("setName:"+setName.getName());
+            Method getName = aClass2.getDeclaredMethod("getName");
+            System.out.println("getName:"+getName.getName());
 
+            Constructor<?>[] declaredConstructors = aClass2.getDeclaredConstructors();
+            for (Constructor constructor : declaredConstructors){
+                System.out.println("constructor:"+constructor.toString());
+            }
+
+            Constructor<?> declaredConstructor = aClass2.getDeclaredConstructor();
+            o = declaredConstructor.newInstance();
+            setName = aClass2.getDeclaredMethod("setName", String.class);
+            setName.setAccessible(true);
+            setName.invoke(o, "aa");
+
+            Method setAge = aClass2.getDeclaredMethod("setAge", int.class);
+            System.out.println("setAge:"+setAge.getName());
+            setAge.invoke(o, 78);
+            System.out.println(o);
+
+            Constructor<?> declaredConstructor1 = aClass2.getDeclaredConstructor(String.class, int.class);
+            Object o1 = declaredConstructor1.newInstance("sd",1);
+            System.out.println(o1);
+
+
+            System.out.println("--------------------------------------------");
+            //String测试
             String s = "Hello";
-//            System.out.println(s.hashCode());
-//            System.out.println(System.identityHashCode(s));
+            System.out.println(s.hashCode());
+            System.out.println(System.identityHashCode(s));
             s = s + " world!";
             System.out.println(s.hashCode());
             System.out.println(System.identityHashCode(s));
--- app/src/main/java/com/ysl/touchlisten/MyListView.java
+++ app/src/main/java/com/ysl/touchlisten/MyListView.java
@@ -42,6 +42,7 @@ public class MyListView extends ListView {
                     getParent().requestDisallowInterceptTouchEvent(false);
                 }
                 break;
+            default:
         }
         return super.dispatchTouchEvent(ev);
     }
