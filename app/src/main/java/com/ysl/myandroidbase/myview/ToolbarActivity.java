79a60b4f76a765bd2eb64706c0f225e882ba24e6
dragon <809629457@qq.com>
2019-07-19 09:46:19 +0800

    toolbar

--- app/src/main/java/com/ysl/myandroidbase/myview/ToolbarActivity.java
+++ app/src/main/java/com/ysl/myandroidbase/myview/ToolbarActivity.java
@@ -1,5 +1,7 @@
 package com.ysl.myandroidbase.myview;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -25,5 +27,14 @@ public class ToolbarActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(view -> {
             Toast.makeText(this, "返回", Toast.LENGTH_SHORT).show();
         });
+
+        //透明状态栏效果
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        }
     }
 }
