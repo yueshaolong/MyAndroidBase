084bb959aaccd0cf10992cb8ff9d0661aada7181
dragon <809629457@qq.com>
2020-09-08 17:48:27 +0800

    拍照加水印

--- app/src/main/java/com/ysl/photo/PhotoActivity.java
+++ app/src/main/java/com/ysl/photo/PhotoActivity.java
@@ -1,6 +1,7 @@
 package com.ysl.photo;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -10,6 +11,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -19,7 +21,6 @@ import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import com.bumptech.glide.Glide;
 import com.tbruyelle.rxpermissions2.RxPermissions;
@@ -27,7 +28,7 @@ import com.ysl.myandroidbase.R;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -39,6 +40,7 @@ public class PhotoActivity extends AppCompatActivity {
     private Button photo;
     private ImageView iv;
     private String path;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,41 +59,69 @@ public class PhotoActivity extends AppCompatActivity {
                             File file = new File(getPictureDirPath().getAbsolutePath(),
                                     System.currentTimeMillis() + ".jpg");
                             path = file.getPath();
-                            Uri imageUri = FileProvider.getUriForFile(getApplicationContext(),
-                                    "com.ysl.myandroidbase.provider", file);
+                            System.out.println("---------->"+path);
+//                            Uri imageUri = FileProvider.getUriForFile(getApplicationContext(),
+//                                    "com.ysl.myandroidbase.provider", file);
+                            imageUri = createImageUri();
                             openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                            openCameraIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                             PhotoActivity.this.startActivityForResult(openCameraIntent, 0);
                         });
                     }
                 });
 
     }
+    private Uri createImageUri() {
+        //设置保存参数到ContentValues中
+        ContentValues contentValues = new ContentValues();
+        //设置文件名
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, System.currentTimeMillis()+"");
+        //兼容Android Q和以下版本
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //android Q中不再使用DATA字段，而用RELATIVE_PATH代替
+            //RELATIVE_PATH是相对路径不是绝对路径;照片存储的地方为：内部存储/Pictures/preventpro
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MyAndroidBase/Photo");
+        } else {
+            contentValues.put(MediaStore.Images.Media.DATA,
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath());
+        }
+        //设置文件类型
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/JPEG");
+        //执行insert操作，向系统文件夹中添加文件
+        //EXTERNAL_CONTENT_URI代表外部存储器，该值不变
+        Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+        return uri;
 
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case 0:
-                if (!TextUtils.isEmpty(path) && resultCode == -1) {
-                    // 获得图片
-                    Bitmap mBitmap = BitmapFactory.decodeFile(path);
-                    //添加时间水印
-                    Bitmap newBitmap = AddTimeWatermark(mBitmap);
-                    saveBitmapFile(newBitmap);
+        try {
+            switch (requestCode) {
+                case 0:
+                    if (!TextUtils.isEmpty(path) && resultCode == -1) {
+                        // 获得图片
+                        Bitmap mBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        //添加时间水印
+                        Bitmap newBitmap = AddTimeWatermark(mBitmap);
+                        saveBitmapFile(newBitmap);
 
-                    Glide.with(getApplicationContext())
-                            .load(path)
-                            .placeholder(R.mipmap.ic_launcher)
-                            .error(R.mipmap.ic_launcher)
-                            .into(iv);
-                }
-                break;
+                        Glide.with(getApplicationContext())
+                                .load(imageUri)
+                                .placeholder(R.mipmap.ic_launcher)
+                                .error(R.mipmap.ic_launcher)
+                                .into(iv);
+                    }
+                    break;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
     public void saveBitmapFile(Bitmap bitmap){
         File file=new File(path);//将要保存图片的路径
         try {
-            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            BufferedOutputStream bos = new BufferedOutputStream(getContentResolver().openOutputStream(imageUri));
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
             bos.flush();
             bos.close();
@@ -134,8 +164,8 @@ public class PhotoActivity extends AppCompatActivity {
     public static File getPictureDirPath() {
         File mIVMSFolder = null;
         try {
-            String path = Environment.getExternalStorageDirectory().getAbsolutePath()
-                    + File.separator + "MyAndroidBase" + File.separator + "Photo";
+            String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                    .getAbsolutePath() + File.separator + "MyAndroidBase" + File.separator + "Photo";
             mIVMSFolder = new File(path);
             if ((null != mIVMSFolder) && (!mIVMSFolder.exists())) {
                 mIVMSFolder.mkdirs();
